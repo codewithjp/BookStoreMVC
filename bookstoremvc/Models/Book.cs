250afabfcb1f64c063cb56dcbced646980022658
jp <jpgujar91@gmail.com>
2021-05-05 07:54:49 +0530

    Install ef core ,ef core sql server,ef core tool @5.0.5 ,setting up the ApplicationDbContext  and Book Model

---
+++ bookstoremvc/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+
+        [MaxLength(100)]
+        public string ISBN { get; set; }
+
+    }
+}
