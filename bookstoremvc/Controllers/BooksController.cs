d612c6167dcba27472ce1af02beaef1ddd67df1e
jp <jpgujar91@gmail.com>
2021-05-05 08:56:39 +0530

    Display List of books  with help of ViewModel

---
+++ bookstoremvc/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreMVC.Models;
+using BookStoreMVC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var bookViewModel = new BookViewModel { Books = _context.Books };
+            return View(bookViewModel);
+        }
+    }
+}
---
+++ bookstoremvc/ViewModel/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.ViewModel
+{
+    public class BookViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
