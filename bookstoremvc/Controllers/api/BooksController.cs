48dd296e2800e5d2fc135cb22a4039db357ad334
jp <jpgujar91@gmail.com>
2021-05-06 08:39:19 +0530

    Applying Jquery DataTable using Api

---
+++ bookstoremvc/Controllers/api/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using BookStoreMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreMVC.Controllers.api
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            return Json(new { data = await _context.Books.ToListAsync() });
+        }
+    }
+}
